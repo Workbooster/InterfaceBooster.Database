b475942f7297f1ced6d1337b406f032c07a4f5eb
Roger Guillet <roger.guillet@workbooster.ch>
2014-12-02 15:21:09 +0100

    renamed the runbuild-log file to "build.log" so the file is ignored by git

--- build/runbuild.cmd
+++ build/runbuild.cmd
@@ -3,6 +3,6 @@
 
 cls
 
-powershell -Command "& { [Console]::WindowWidth = 150; [Console]::WindowHeight = 50; Start-Transcript runbuild.txt; Import-Module ..\Tools\PSake\psake.psm1; Invoke-psake ..\Build\build.ps1 %*; Stop-Transcript; }"
+powershell -Command "& { [Console]::WindowWidth = 150; [Console]::WindowHeight = 50; Start-Transcript build.log; Import-Module ..\Tools\PSake\psake.psm1; Invoke-psake ..\Build\build.ps1 %*; Stop-Transcript; }"
 
 pause
